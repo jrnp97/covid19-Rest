1573a5e0defb505e3cbed8307b3f27adad8d9259
jrnp97 <jrnp1997@gmail.com>
2020-03-21 01:08:12 -0400

    Register app

--- covid_19/settings.py
+++ covid_19/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+from covid_19.settings_local import *
