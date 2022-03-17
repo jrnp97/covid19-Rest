1e193b95f343add366e657ad1225900d9ff12195
jrnp97 <jrnp1997@gmail.com>
2022-03-16 22:28:48 -0300

    build: Include missing dependencies

--- covid_19/settings.py
+++ covid_19/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +131,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # CELERY CONFIG
 CELERY_RESULT_BACKEND = 'django-db'
