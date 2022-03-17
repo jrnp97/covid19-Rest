e029732ed673f21f8febc8f280412c6b888be8a7
jrnp97 <jrnp1997@gmail.com>
2022-03-16 22:31:46 -0300

    fix: allowed_hosts settings

--- covid_19/settings.py
+++ covid_19/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vhri!8r$wk&hv7y$((xvc%draow!$kllqo#wqx^yk&-0ipqh6m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
